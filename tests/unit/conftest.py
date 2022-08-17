4f2580c1f9cfa97be1b5bb1452c07e3a61b1b904
DelphinePythonique <delphine.lemire+openclassroom@gmail.com>
2022-08-17 16:19:15 +0200

    BUG:email unknow finalization of unit tests

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -10,33 +10,35 @@ def client():
     with app.test_client() as client:
         yield client
 
+
 @pytest.fixture
 def clubs_fixture():
     data = [
-    {
-        "name":"Simply Lift",
-        "email":"john@gudlft.ok",
-        "points":"13"
-    },
-    {
-        "name":"Iron Temple",
-        "email": "admin@irontemple.com",
-        "points":"4"
-    },
-    {   "name":"She Lifts 1",
-        "email": "kate@shelifts.co.uk",
-        "points":"12"
-    }
-]
+        {"name": "Simply Lift", "email": "john@gudlft.ok", "points": "13"},
+        {"name": "Iron Temple", "email": "admin@irontemple.com", "points": "4"},
+        {"name": "She Lifts 1", "email": "kate@shelifts.co.uk", "points": "12"},
+    ]
     return data
+
 
 @pytest.fixture
 def club_fixture():
-    data = [
-    {
-        "name":"Simply Lift",
-        "email":"john@gudlft.ok",
-        "points":"13"
-    }
-]
+    data = [{"name": "Simply Lift", "email": "john@gudlft.ok", "points": "13"}]
     return data
+
+
+@pytest.fixture
+def clubs_schema_fixture():
+    schema = {
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"},
+                "email": {"type": "string"},
+                "points": {"type": "string"},
+            },
+            "required": ["name", "email", "points"],
+        },
+    }
+    return schema
