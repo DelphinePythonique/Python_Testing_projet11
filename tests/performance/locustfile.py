9551b25bbe378e2211cd36e406bb029f0a39ce02
DelphinePythonique <delphine.lemire+openclassroom@gmail.com>
2022-08-31 09:45:45 +0200

    BUG #3: FIX points_allowed with tests

--- tests/performance/locustfile.py
+++ tests/performance/locustfile.py
@@ -1,6 +1,6 @@
 from locust import HttpUser, task, between
 
-from tests.conftest import EMAIL_OK
+from tests.conftest import EMAIL_OK, CLUB_OK, COMPETITION_OK
 
 
 class ServerPerfTest(HttpUser):
@@ -11,5 +11,13 @@ class ServerPerfTest(HttpUser):
         self.client.get("/")
 
     @task
-    def showSummary(self):
+    def show_summary(self):
         self.client.post("/showSummary", {"email": EMAIL_OK})
+
+    @task(8)
+    def book(self):
+        self.client.get("/book/competition test1/test1")
+
+    @task(5)
+    def purchase_place(self):
+        self.client.post("/purchasePlaces", {"club": CLUB_OK, "competition": COMPETITION_OK, "places": 1})
